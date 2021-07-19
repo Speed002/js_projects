5b695cb3e6a0640ff2c3ec642ffd2f463186e484
Speed <speedwasswa02@gmail.com>
2021-07-19 13:32:49 +0300

    custom video player completed

---
+++ custom-video-player/main.js
@@ -0,0 +1,62 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play & pause video
+function toggleVideoStatus(){
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
+
+// update play/pause icon
+function updatePlayIcon(){
+    if(video.paused){
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    }else{
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'; 
+    }
+}
+
+// Update progress & timestamp
+function updateProgress(){
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    //get the minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10){
+        mins = '0' + String(mins);
+    }
+
+    //get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10){
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+function stopVideo(){
+    video.currentTime = 0;
+    video.pause();
+}
+
+function setVideoProgress(){
+    video.currentTime = (+progress.value * video.duration) / 100;
+
+}
+
+//Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+progress.addEventListener('change', setVideoProgress);
+stop.addEventListener('click', stopVideo);
